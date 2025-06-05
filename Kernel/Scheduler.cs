8b28f82ca90d70e72465524f975bdb3f861e47e1
Hakurei <159775249+atelieralice@users.noreply.github.com>
2025-06-05 15:47:30 +0300

    Implement Round Robin scheduler

--- Kernel/Scheduler.cs
+++ Kernel/Scheduler.cs
@@ -1,6 +1,7 @@
 using AlkaOS.Kernel.Scheduling;
 using Godot;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace AlkaOS.Kernel.Scheduling;
@@ -13,10 +14,33 @@ public interface IScheduler {
 }
 
 public class RoundRobinScheduler : IScheduler {
-    void AddProcess ( PCB process ) {
-        
+    public readonly Queue<PCB> readyQueue = new Queue<PCB>();
+
+    public void AddProcess ( PCB process ) {
+        readyQueue.Enqueue ( process );
     }
-    void RemoveProcess ( int pid );
-    PCB GetNextProcess ( );
-    IEnumerable<PCB> GetReadyQueue ( );
+
+    public void RemoveProcess ( int pid ) {
+        Queue<PCB> tempQueue = new Queue<PCB> ( );
+        while ( readyQueue.Count > 0 ) {
+            PCB pcb = readyQueue.Dequeue ( );
+            if ( pcb.ProcessID != pid )
+                tempQueue.Enqueue ( pcb );
+        }
+        readyQueue.Clear();
+        while (tempQueue.Count > 0)
+            readyQueue.Enqueue(tempQueue.Dequeue());
+    }
+
+    public PCB GetNextProcess ( ) {
+        if ( readyQueue.Count == 0 )
+            return null;
+        PCB pcb = readyQueue.Dequeue();
+        readyQueue.Enqueue ( pcb );
+        return pcb;
+    }
+
+    public IEnumerable<PCB> GetReadyQueue ( ) {
+        return readyQueue.ToArray ( );
+    }
 }
